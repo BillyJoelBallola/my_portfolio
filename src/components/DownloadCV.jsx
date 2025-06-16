17f85188b0a05dcb34d94ba43bf191e9451a3be5
billy <billyjoelballola27@gmail.com>
2025-06-17 01:06:32 +0800

    edit theme and downloadcv

--- src/components/DownloadCV.jsx
+++ src/components/DownloadCV.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { GrDocumentUser } from "react-icons/gr";
+import { IoMdClose } from "react-icons/io";
 import cvFile from "../assets/BALLOLA_BILLY_JOEL_RESUME.pdf";
 
 const DownloadCV = () => {
+  const [showToolTip, setShowToolTip] = useState(true);
+
   return (
     <div className="group grid place-content-center fixed bottom-8 md:bottom-16 left-5 lg:left-10 bg-gray-100 dark:bg-slate-800 shadow-md rounded-full w-10 aspect-square z-40">
-      <div className="hidden group-hover:block absolute whitespace-nowrap -top-3 left-9 rounded-md px-2 py-1 z-20 bg-white dark:bg-slate-600 dark:text-white shadow-md border dark:border-0 text-gray-500">
+      {/* <div className="hidden group-hover:block absolute whitespace-nowrap -top-3 left-9 rounded-md px-2 py-1 z-20 bg-white dark:bg-slate-600 dark:text-white shadow-md border dark:border-0 text-gray-500">
         <span className="text-sm">Download CV</span>
-      </div>
+      </div> */}
+      {showToolTip && (
+        <div
+          data-aos="zoom-in-right"
+          className="flex gap-8 absolute whitespace-nowrap -top-8 left-9 rounded-md px-2 py-1 z-20 bg-orange-500 dark:bg-sky-600 text-white shadow-md border dark:border-0"
+        >
+          <span className="text-sm">
+            You can download my CV
+            <br />
+            just by clicking the icon.
+          </span>
+          <button onClick={() => setShowToolTip(false)}>
+            <IoMdClose />
+          </button>
+        </div>
+      )}
       <a
         href={cvFile}
         className="text-orange-500 dark:text-sky-500 text-xl cursor-pointer"
--- src/components/Theme.jsx
+++ src/components/Theme.jsx
@@ -1,28 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { BsMoonFill, BsSunFill } from "react-icons/bs";
-
-const ActiveIcon = ({ isDarkMode }) => {
-  const activeTheme = localStorage.getItem("colorTheme");
-  let result = null;
-
-  const moon = <BsMoonFill className="text-sky-500" />;
-  const sun = <BsSunFill className="text-orange-500" />;
-
-  if (activeTheme === "dark" && isDarkMode === true) {
-    result = moon;
-  } else if (activeTheme === "light" && isDarkMode === false) {
-    result = sun;
-  } else if (activeTheme === "dark" && isDarkMode === false) {
-    result = moon;
-  } else if (activeTheme === "light" && isDarkMode === true) {
-    result = sun;
-  }
-
-  return result;
-};
+import React, { useState } from "react";
+import { LuSunMedium } from "react-icons/lu";
+import { PiMoonStars } from "react-icons/pi";
 
 const Theme = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const activeTheme = localStorage.getItem("colorTheme");
 
   if (localStorage.getItem("colorTheme") === "dark") {
     document.documentElement.classList.add(localStorage.getItem("colorTheme"));
@@ -48,10 +30,10 @@ const Theme = () => {
   return (
     <button
       data-aos="zoom-in"
-      className="shadow-md fixed z-50 right-5 lg:right-10 top-8 text-2xl bg-[#efefeffb] dark:bg-slate-800 p-2 rounded-full duration-200"
+      className="shadow-sm fixed z-50 right-5 lg:right-10 top-8 text-2xl bg-orange-500 dark:bg-sky-500 text-orange-100 dark:text-sky-950 p-2 rounded-full duration-200"
       onClick={turnOnDarkMode}
     >
-      <ActiveIcon isDarkMode={isDarkMode} />
+      {activeTheme === "light" ? <LuSunMedium /> : <PiMoonStars />}
     </button>
   );
 };
